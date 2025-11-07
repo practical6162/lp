803de574691dcfc95621b8f514ebd533bb4fb168
pr <practical6162@gmail.com>
2025-11-07 23:00:13 +0530

    Update pr12.java

--- pr12.java
+++ pr12.java
@@ -1,53 +1,48 @@
-import java.util.*;
+use collegeDB
 
-public class LRUPageReplacement {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+db.Teachers.insertMany([
+  { Tname: "Amit", dno: 1, Experience: 5, Salary: 12000, Date_of_Joining: "2018-06-12" },
+  { Tname: "Ravi", dno: 2, Experience: 8, Salary: 15000, Date_of_Joining: "2015-03-25" },
+  { Tname: "Sneha", dno: 2, Experience: 3, Salary: 9500, Date_of_Joining: "2020-01-10" },
+  { Tname: "Priya", dno: 3, Experience: 10, Salary: 18000, Date_of_Joining: "2012-09-05" }
+])
 
-        System.out.print("Enter number of frames: ");
-        int frames = sc.nextInt();
 
-        System.out.print("Enter length of page reference string: ");
-        int n = sc.nextInt();
+db.Department.insertMany([
+  { Dno: 1, Dname: "Computer" },
+  { Dno: 2, Dname: "IT" },
+  { Dno: 3, Dname: "ENTC" }
+])
 
-        int[] pages = new int[n];
-        System.out.println("Enter the page reference string:");
-        for (int i = 0; i < n; i++) {
-            pages[i] = sc.nextInt();
-        }
 
-        ArrayList<Integer> frameList = new ArrayList<>(frames);
-        int pageFaults = 0;
+db.Students.insertMany([
+  { Sname: "abc", Roll_No: 1, Class: "FE" },
+  { Sname: "xyz", Roll_No: 2, Class: "SE" },
+  { Sname: "pqr", Roll_No: 3, Class: "TE" },
+  { Sname: "mno", Roll_No: 5, Class: "FE" }
+])
 
-        System.out.println("\nPage\tFrames");
-        System.out.println("--------------------------");
 
-        for (int page : pages) {
-            // If page not present in memory
-            if (!frameList.contains(page)) {
-                if (frameList.size() < frames) {
-                    frameList.add(page);
-                } else {
-                    // Remove least recently used (first element)
-                    frameList.remove(0);
-                    frameList.add(page);
-                }
-                pageFaults++;
-            } else {
-                // If page is already in memory, move it to most recently used position
-                frameList.remove((Integer) page);
-                frameList.add(page);
-            }
+db.Teachers.find(
+  { dno: 2, Salary: { $gte: 10000 } }
+)
 
-            // Display frame contents
-            System.out.print(page + "\t");
-            for (int p : frameList) {
-                System.out.print(p + " ");
-            }
-            System.out.println();
-        }
 
-        System.out.println("\nTotal Page Faults = " + pageFaults);
-        sc.close();
-    }
-}
+db.Students.find(
+  { $or: [ { Roll_No: 2 }, { Sname: "xyz" } ] }
+)
+
+
+db.Students.updateOne(
+  { Roll_No: 5 },
+  { $set: { Sname: "Updated_Student" } }
+)
+
+
+db.Students.deleteMany({ Class: "FE" })
+
+
+db.Students.createIndex({ Roll_No: 1 })
+
+
+db.Students.getIndexes()
