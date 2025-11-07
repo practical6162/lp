09dad55c6d3a1f3ba733698cacc412663da7415a
pr <practical6162@gmail.com>
2025-11-07 22:57:18 +0530

    Update pr11.java

--- pr11.java
+++ pr11.java
@@ -1,52 +1,54 @@
-import java.util.*;
+use moviedb
 
-public class FIFOPageReplacement {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter number of frames: ");
-        int frames = sc.nextInt();
-
-        System.out.print("Enter length of page reference string: ");
-        int n = sc.nextInt();
-
-        int[] pages = new int[n];
-        System.out.println("Enter the page reference string:");
-        for (int i = 0; i < n; i++) {
-            pages[i] = sc.nextInt();
-        }
-
-        Queue<Integer> queue = new LinkedList<>();
-        Set<Integer> set = new HashSet<>();
-        int pageFaults = 0;
-
-        System.out.println("\nPage\tFrames");
-        System.out.println("--------------------------");
-
-        for (int page : pages) {
-            // Page not in memory → page fault
-            if (!set.contains(page)) {
-                if (set.size() < frames) {
-                    set.add(page);
-                    queue.add(page);
-                } else {
-                    int oldest = queue.poll();
-                    set.remove(oldest);
-                    set.add(page);
-                    queue.add(page);
-                }
-                pageFaults++;
-            }
-
-            // Display current frames
-            System.out.print(page + "\t");
-            for (int p : queue) {
-                System.out.print(p + " ");
-            }
-            System.out.println();
-        }
-
-        System.out.println("\nTotal Page Faults = " + pageFaults);
-        sc.close();
+db.movies.insertMany([
+  {
+    name: "Movie1",
+    type: "action",
+    budget: 1000000,
+    producer: {
+      name: "producer1",
+      address: "PUNE"
     }
-}
+  },
+  {
+    name: "Movie2",
+    type: "comedy",
+    budget: 500000,
+    producer: {
+      name: "producer2",
+      address: "MUMBAI"
+    }
+  },
+  {
+    name: "Movie3",
+    type: "action",
+    budget: 2000000,
+    producer: {
+      name: "producer1",
+      address: "PUNE"
+    }
+  }
+])
+
+
+db.movies.find(
+  { budget: { $gt: 100000 } },
+  { _id: 0, name: 1 }
+)
+
+db.movies.find(
+  { "producer.address": "PUNE" },
+  { _id: 0, "producer.name": 1 }
+)
+
+db.movies.updateMany(
+  { type: "action" },
+  { $set: { type: "horror" } }
+)
+
+db.movies.find({}, { _id: 0, name: 1, type: 1 })
+
+db.movies.find(
+  { "producer.name": "producer1" },
+  { _id: 0, name: 1, "producer.address": 1 }
+)
