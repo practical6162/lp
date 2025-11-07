3c8cd3b827ede829d57c4e1076b4bcb21768cef5
pr <practical6162@gmail.com>
2025-11-07 23:19:32 +0530

    Update pr9.java

--- pr9.java
+++ pr9.java
@@ -1,79 +1,54 @@
-import java.util.*;
+CREATE DATABASE librarydb;
+USE librarydb;
 
-public class NextFit {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+CREATE TABLE Library (
+    Lib_ID INT PRIMARY KEY,
+    Book_Name VARCHAR(50),
+    Author VARCHAR(50),
+    Price DECIMAL(8,2)
+);
 
-        System.out.print("Enter number of memory blocks: ");
-        int nb = sc.nextInt();
-        int[] blockSize = new int[nb];
-        int[] blockAllocated = new int[nb];
+CREATE TABLE Library_Audit (
+    Audit_ID INT AUTO_INCREMENT PRIMARY KEY,
+    Lib_ID INT,
+    Book_Name VARCHAR(50),
+    Author VARCHAR(50),
+    Price DECIMAL(8,2),
+    Action_Type VARCHAR(20),
+    Action_Date DATETIME
+);
+INSERT INTO Library VALUES
+(101, 'DBMS', 'Ramakrishnan', 550.00),
+(102, 'Operating System', 'Galvin', 600.00),
+(103, 'Computer Networks', 'Tanenbaum', 700.00);
+DELIMITER $$
 
-        System.out.println("Enter size of each memory block:");
-        for (int i = 0; i < nb; i++) {
-            System.out.print("Block " + (i + 1) + ": ");
-            blockSize[i] = sc.nextInt();
-            blockAllocated[i] = -1; // not allocated
-        }
+CREATE TRIGGER trg_after_update_Library
+AFTER UPDATE ON Library
+FOR EACH ROW
+BEGIN
+    INSERT INTO Library_Audit (Lib_ID, Book_Name, Author, Price, Action_Type, Action_Date)
+    VALUES (OLD.Lib_ID, OLD.Book_Name, OLD.Author, OLD.Price, 'UPDATED', NOW());
+END$$
 
-        System.out.print("\nEnter number of processes: ");
-        int np = sc.nextInt();
-        int[] processSize = new int[np];
-        int[] allocation = new int[np];
-        Arrays.fill(allocation, -1);
+DELIMITER ;
+DELIMITER $$
 
-        System.out.println("Enter size of each process:");
-        for (int i = 0; i < np; i++) {
-            System.out.print("Process " + (i + 1) + ": ");
-            processSize[i] = sc.nextInt();
-        }
+CREATE TRIGGER trg_after_delete_Library
+AFTER DELETE ON Library
+FOR EACH ROW
+BEGIN
+    INSERT INTO Library_Audit (Lib_ID, Book_Name, Author, Price, Action_Type, Action_Date)
+    VALUES (OLD.Lib_ID, OLD.Book_Name, OLD.Author, OLD.Price, 'DELETED', NOW());
+END$$
 
-        int lastAllocated = 0;
+DELIMITER ;
 
-        // Next Fit Allocation
-        for (int i = 0; i < np; i++) {
-            int count = 0;
-            boolean allocated = false;
+UPDATE Library SET Price = 650 WHERE Lib_ID = 102;
 
-            while (count < nb) {
-                int j = (lastAllocated + count) % nb;
-                if (blockAllocated[j] == -1 && blockSize[j] >= processSize[i]) {
-                    allocation[i] = j;
-                    blockAllocated[j] = i;
-                    lastAllocated = j; // start next search from here
-                    allocated = true;
-                    break;
-                }
-                count++;
-            }
+DELETE FROM Library WHERE Lib_ID = 103;
 
-            if (!allocated) {
-                allocation[i] = -1;
-            }
-        }
 
-        System.out.println("\n------------------------------------------------------------");
-        System.out.println("Process No.\tProcess Size\tBlock Allocated");
-        System.out.println("------------------------------------------------------------");
-        for (int i = 0; i < np; i++) {
-            if (allocation[i] != -1)
-                System.out.println((i + 1) + "\t\t" + processSize[i] + "\t\tBlock " + (allocation[i] + 1));
-            else
-                System.out.println((i + 1) + "\t\t" + processSize[i] + "\t\tNot Allocated");
-        }
+--final display
+SELECT * FROM Library_Audit;
 
-        System.out.println("\n------------------------------------------------------------");
-        System.out.println("Block No.\tBlock Size\tAllocated\tFragment");
-        System.out.println("------------------------------------------------------------");
-        for (int j = 0; j < nb; j++) {
-            if (blockAllocated[j] != -1) {
-                int frag = blockSize[j] - processSize[blockAllocated[j]];
-                System.out.println((j + 1) + "\t\t" + blockSize[j] + "\t\tP" + (blockAllocated[j] + 1) + "\t\t" + frag);
-            } else {
-                System.out.println((j + 1) + "\t\t" + blockSize[j] + "\t\tFree\t\t" + blockSize[j]);
-            }
-        }
-
-        sc.close();
-    }
-}
