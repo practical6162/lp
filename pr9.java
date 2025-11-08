6457e866950512c8036baca1a36e03b1f03798ad
pr <practical6162@gmail.com>
2025-11-08 07:08:57 +0530

    Update pr9.java

--- pr9.java
+++ pr9.java
@@ -1,54 +1,46 @@
-CREATE DATABASE librarydb;
-USE librarydb;
-
 CREATE TABLE Library (
-    Lib_ID INT PRIMARY KEY,
-    Book_Name VARCHAR(50),
-    Author VARCHAR(50),
-    Price DECIMAL(8,2)
+ Book_ID NUMBER PRIMARY KEY,
+ Title VARCHAR2(50),
+ Author VARCHAR2(50),
+ Price NUMBER(8,2)
 );
-
 CREATE TABLE Library_Audit (
-    Audit_ID INT AUTO_INCREMENT PRIMARY KEY,
-    Lib_ID INT,
-    Book_Name VARCHAR(50),
-    Author VARCHAR(50),
-    Price DECIMAL(8,2),
-    Action_Type VARCHAR(20),
-    Action_Date DATETIME
+ Book_ID NUMBER,
+ Title VARCHAR2(50),
+ Author VARCHAR2(50),
+ Price NUMBER(8,2),
+ Operation_Type VARCHAR2(10),
+ Operation_Date DATE
 );
-INSERT INTO Library VALUES
-(101, 'DBMS', 'Ramakrishnan', 550.00),
-(102, 'Operating System', 'Galvin', 600.00),
-(103, 'Computer Networks', 'Tanenbaum', 700.00);
-DELIMITER $$
-
-CREATE TRIGGER trg_after_update_Library
-AFTER UPDATE ON Library
+INSERT INTO Library VALUES (1, 'DBMS Concepts', 'Ramakrishnan', 550);
+INSERT INTO Library VALUES (2, 'Operating Systems', 'Galvin', 650);
+INSERT INTO Library VALUES (3, 'Computer Networks', 'Tanenbaum', 600);
+COMMIT;
+SQL> edit trg_library_audit.sql
+CREATE OR REPLACE TRIGGER trg_library_audit
+BEFORE UPDATE OR DELETE
+ON Library
 FOR EACH ROW
 BEGIN
-    INSERT INTO Library_Audit (Lib_ID, Book_Name, Author, Price, Action_Type, Action_Date)
-    VALUES (OLD.Lib_ID, OLD.Book_Name, OLD.Author, OLD.Price, 'UPDATED', NOW());
-END$$
-
-DELIMITER ;
-DELIMITER $$
-
-CREATE TRIGGER trg_after_delete_Library
-AFTER DELETE ON Library
-FOR EACH ROW
-BEGIN
-    INSERT INTO Library_Audit (Lib_ID, Book_Name, Author, Price, Action_Type, Action_Date)
-    VALUES (OLD.Lib_ID, OLD.Book_Name, OLD.Author, OLD.Price, 'DELETED', NOW());
-END$$
-
-DELIMITER ;
-
-UPDATE Library SET Price = 650 WHERE Lib_ID = 102;
-
-DELETE FROM Library WHERE Lib_ID = 103;
-
-
---final display
-SELECT * FROM Library_Audit;
-
+ INSERT INTO Library_Audit
+ (Book_ID, Title, Author, Price, Operation_Type, Operation_Date)
+ VALUES
+ (:OLD.Book_ID, :OLD.Title, :OLD.Author, :OLD.Price,
+ CASE
+ WHEN UPDATING THEN 'UPDATE'
+ WHEN DELETING THEN 'DELETE'
+ END,
+ SYSDATE);
+END;
+/
+SQL> @trg_library_audit.sql
+----------------------------------------------------------------------------
+UPDATE Library
+SET Price = 700
+WHERE Book_ID = 2;
+-----------------------------------------------------------------------------------
+DELETE FROM Library
+WHERE Book_ID = 3;
+------------------------------------------------------------------------------------
+SQL> SELECT * FROM Library_Audit;
+*******************************
